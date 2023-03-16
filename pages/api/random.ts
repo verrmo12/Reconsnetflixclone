b2f67b89d5562cca3b47c5369ec452f982d5e618
verrmo12 <bouchrasouhni@gmail.com>
2023-03-16 20:52:26 +0100

    final

---
+++ pages/api/random.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prismadb from '@/libs/prismadb';
+import serverAuth from "@/libs/serverAuth";
+
+export default async function handler(req: NextApiRequest, res:NextApiResponse){
+    if (req.method !== 'GET') {
+        return res.status(405).end();
+    }
+
+    try{
+        await serverAuth(req);
+
+        const movieCount = await prismadb.movie.count();
+        const randomIdex = Math.floor(Math.random() * movieCount);
+
+        const randomMovies = await prismadb.movie.findMany({
+            take: 1,
+            skip: randomIdex
+        });
+
+        return res.status(200).json(randomMovies[0]);
+    }catch (error){
+        console.log(error);
+        return res.status(400).end();
+    }
+}
