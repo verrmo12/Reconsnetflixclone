6786236bb83d116feb65830848be942ccd1f9bab
verrmo12 <bouchrasouhni@gmail.com>
2023-03-16 22:30:33 +0100

    Updated code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
-import Navbar from "../components/navbar";
+
 
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
@@ -8,6 +8,7 @@ import useMovieList from "@/hooks/useMovieList";
 import useFavorites from "@/hooks/useFavorites";
 import InfoModal from "@/components/InfoModal";
 import useInfoModal from "@/hooks/useInfoModal";
+import Navbar from "@/components/Navbar";
 
 
 export async function getServerSideProps(context: NextPageContext) {
